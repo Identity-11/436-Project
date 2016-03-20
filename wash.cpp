ee7cb698fcd5378deb708cebc4d95ee0bf2fe141
JoshKorth <derkorth@gmail.com>
2016-03-20 14:25:08 -0700

    Update wash.cpp
    Change Method

--- wash.cpp
+++ wash.cpp
@@ -1,150 +1,182 @@
-// This #include statement was automatically added by the Particle IDE.
 #include "SparkFunLSM9DS1/SparkFunLSM9DS1.h"
 #include "math.h"
 
-#define HELLO_WORLD 1
-#define LOOP_INTERVAL           3000
-#define EMAIL_INTERVAL          15000
+#define LOOP_INTERVAL         1000 /* In Milliseconds */
+#define EMAIL_INTERVAL        60   /* In Seconds */
 
-#define TUCSON_MAGNETIC_PULL    9.59
+#define TUCSON_MAGNETIC_PULL  9.59 /* In m/s */
 
-#define AWAITING_START_STATE 1
-#define WASHING_STATE        2
-#define AWAITING_DOOR_STATE  3
+#define AWAITING_START_STATE  1
+#define WASHING_STATE         2
+#define AWAITING_DOOR_STATE   3
 
-#define DEBUG_STATE 1
+#define DEBUG_STATE           1
 
+#define MOTION_PADDING        10
 
-LSM9DS1 accel;
-int state;
-int motionCounter;
-int hasStarted;
-int hasEnded;
-int hasDoorOpened;
-int curMin;
-int totalTime = 0;
-int prevX, prevY,prevZ;
+LSM9DS1 imu;
 
-void threshold (int x, int y, int z);
+float calcMagnitude();
+float findRestingChange();
+byte   threshold();
 
-/* Setup once device has powered on */
+int 	currentState;
+
+long 	totalTime 	= 0;
+int 	windowStart = 0;
+
+byte 	window[60];
+
+float 	lastMagnitude = 0.0F;
+float	restingMagnitudeChange;
+
 void setup()
 {
-   Serial.begin(115200);
-   
-   accel.settings.device.commInterface = IMU_MODE_I2C;
-   accel.settings.device.agAddress     = 0x6B;
-   accel.settings.device.mAddress      = 0x1E;
-   
-   state = AWAITING_START_STATE;
-   
-   while (1) {}
+	Serial.begin(115200);
+
+	imu.settings.device.commInterface = 	IMU_MODE_I2C;
+    imu.settings.device.agAddress     = 	0x6B;
+    imu.settings.device.mAddress      = 	0x1E;
+
+    imu.begin();
+
+    currentState = AWAITING_START_STATE;
+
+    restingMagnitudeChange = findRestingChange();
+
+    for (int i = 0; i < 60; i++)
+    {
+    	window[i] = 0;
+    }
+
+    while (1)
+    {
+    	loop();
+    	delay(LOOP_INTERVAL);
+    }
 }
 
-/* Main Loop */
 void loop()
 {
-   if (state == AWAITING_START_STATE)
-   {
-      curMin = Time.minute();
-      while ( abs(curMin - Time.minute()) < 1) {
-         threshold(accel.ax, accel.ay, accel.az);
-      }
-      
-      if (motionCounter >= 50)
-         hasStarted = 1;
-      
-      if (hasStarted == 1)
-      {
-         state = WASHING_STATE;
-      }
-   }
-   else if (state == WASHING_STATE)
-   {
-      curMin = Time.minute();
-      
-      while ( abs(curMin - Time.minute()) < 30) {
-         threshold(accel.ax, accel.ay, accel.az);
-      }
-      totalTime = totalTime + (abs( curMin - Time.minute() ));
-      
-      if (motionCounter >= 2000)
-         hasEnded = 1;
-      
-      if (hasEnded == 1)
-      {
-         state = AWAITING_DOOR_STATE;
-      }
-      
-   }
-   else if (state == AWAITING_DOOR_STATE)
-   {
-      curMin = Time.minute();
-      sendEvent();
-      
-      while ( abs(curMin - Time.minute()) < 30) {
-         if ( motionCounter >= 20 ) {
-            hasDoorOpened = 1;
-            break;
-         }
-         threshold(accel.ax, accel.ay, accel.az);
-      }
-      totalTime = totalTime + (abs( curMin - Time.minute() ));
-      
-      
-      if (hasDoorOpened == 1)
-      {
-         state = AWAITING_START_STATE;
-      }
-   }
+	if (currentState == AWAITING_START_STATE)
+	{
+		int i;
+		int sum = 0;
+
+		for (i = 0; i < 59; i++)
+		{
+			sum += window[i];
+			window[i] = window[i+1];
+		}
+		window[59] = threshold();
+		sum += window[59];
+
+		if (sum >= 45) /* 75% of the last minute detected motion */
+		{
+			currentState = WASHING_STATE;
+			for (i = 0; i < 60; i++)
+			{
+				window[i] = 1;
+			}
+		}
+	}
+
+	if (currentState == WASHING_STATE)
+	{
+		int i = 0;
+		int sum = 0;
+
+		for (i = 0; i < 59; i++)
+		{
+			sum += window[i];
+			window[i] = window[i+1];
+		}
+		window[59] = threshold();
+		sum += window[59];
+
+		if (sum <= 10) /* 83.33% of last minute, no motion detected */
+		{ 
+			currentState = AWAITING_DOOR_STATE;
+			totalTime = Time.now();
+			for (i = 0; i < 60; i++)
+			{
+				window[i] = 0;
+			}
+		}
+	}
+
+	if (currentState == AWAITING_DOOR_STATE)
+	{
+		if ((Time.now() - totalTime) >= EMAIL_INTERVAL)
+		{
+			totalTime = Time.now();
+			sendEvent();
+		}
+
+		int i = 0;
+		int sum = 0;
+
+		for (i = 0; i < 59; i++)
+		{
+			sum += window[i];
+			window[i] = window[i+1];
+		}
+		window[59] = threshold();
+		sum += window[59];
+
+		if (sum >= 10) /* Subject to Change */
+		{
+			totalTime = 0;
+			state = AWAITING_START_STATE;
+		}
+	}
 }
 
-/* Debug Serial Printing */
-void debugOutput()
+byte threshold()
 {
-   accel.readAccel();
-   
-   char debugBuffer[512];
-   bzero(debugBuffer, 512);
-   
-   sprintf(debugBuffer, "Time: %lu\nAx: %d | Ay: %d | Az: %d\n", Time.now(),  accel.ax, accel.ay, accel.az);
-   
-   Serial.print("--------------------\n");
-   Serial.print(debugBuffer);
-   Serial.print("--------------------\n");
+	imu.readAccel();
+
+	if (abs(calcMagnitude() - lastMagnitude) >= (restingMagnitudeChange + MOTION_PADDING))
+	{
+		motionCounter++;
+		return 1;
+	}
+	return 0;
 }
 
-/* Publish Event to E-Mail Customer */
-void sendEvent()
+float findRestingChange()
 {
-   /* Publish Code To Use Later */
-   // char bufferX[100];
-   // char bufferY[100];
-   // char bufferZ[100];
-   // char bufferS[100];
-   // snprintf(bufferX, sizeof(char) * 32, "%d", imu.ax);
-   // snprintf(bufferY, sizeof(char) * 32, "%d", imu.ay);
-   // snprintf(bufferZ, sizeof(char) * 32, "%d", imu.az);
-   // snprintf(bufferS, sizeof(char) * 32, "%d", (imu.ax + imu.ay + imu.az));
-   // snprintf(&buffer[32], sizeof(char) * 32, "%d", imu.ay);
-   // snprintf(&buffer[65], sizeof(char) * 32, "%d", imu.az);
-   // Spark.publish("Ax", bufferX, 60, PRIVATE);
-   // Spark.publish("Ay", bufferY, 60, PRIVATE);
-   // Spark.publish("Az", bufferZ, 60, PRIVATE);
-   // Spark.publish("SumA", bufferS, 60, PRIVATE);
+	int i;
+	float totalMagnitudeChange = 0.0F;
+
+	for (i = 0; i < (60000 / LOOP_INTERVAL); i++)
+	{
+		imu.readAccel();
+
+		if (lastMagnitude != 0.0F)
+		{
+			lastMagnitude = calcMagnitude();
+		}
+		else
+		{
+			float currMagnitude = calcMagnitude();
+			totalMagnitudeChange += abs(currMagnitude - lastMagnitude);
+			lastMagnitude = currMagnitude;
+		}
+	}
+	return totalMagnitudeChange / (i - 1);
 }
 
-void threshold(int x, int y, int z) {
-   if (prevX == 0 && prevY == 0 && prevZ == 0) {
-      prevX = x;
-      prevY = y;
-      prevZ = z;
-      return;
-   } else {
-      if ( abs( (x+y+z) - (prevX+prevY+prevZ) ) > 300)
-         motionCounter++;
-   }
-   prevX = x;
-   prevY = y;
-   prevZ = z;
+float calcMagnitude()
+{
+	imu.readAccel();
+	float x = imu.calcAccel(imu.ax);
+	float y = imu.calcAccel(imu.ay);
+	float z = imu.calcAccel(imu.az);
+
+	x = x * x;
+	y = y * y;
+	z = z * z;
+
+	return sqrt(x + y + z) * 1000;
 }
